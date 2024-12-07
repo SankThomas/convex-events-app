b9bd5e68de2c7701c3841efb0133d38d92103158
SankThomas <thomassankara74@gmail.com>
2024-12-07 12:24:29 +0300

    update loading icon

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,10 +1,11 @@
+import { Loader2 } from "lucide-react";
 import { Loader } from "lucide-react";
 import React from "react";
 
 export default function Loading() {
   return (
     <div className="mx-auto mt-10 max-w-4xl">
-      <Loader className="mx-auto block size-9 animate-spin text-neutral-400" />
+      <Loader2 className="mx-auto block size-9 animate-spin text-neutral-400" />
     </div>
   );
 }
