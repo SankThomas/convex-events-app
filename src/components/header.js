3679d620a0c4f08c2e648cf7ab45058ace51c93c
SankThomas <thomassankara74@gmail.com>
2024-12-07 13:12:14 +0300

    add logout dialog and kinde user session details

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,20 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { Button } from "./ui/button";
 import { Home } from "lucide-react";
+import {
+  LogoutLink,
+  useKindeBrowserClient,
+} from "@kinde-oss/kinde-auth-nextjs";
+import { ChevronDown } from "lucide-react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 const links = [
   {
@@ -19,13 +33,41 @@ const links = [
 
 export default function Header() {
   const pathname = usePathname();
+  const { user } = useKindeBrowserClient();
 
   return (
     <header className="fixed left-1/2 z-10 mx-auto mt-6 w-full max-w-4xl -translate-x-1/2 rounded-full border border-neutral-700 bg-neutral-800/50 px-6 py-3 backdrop-blur-md">
       <div className="flex flex-wrap items-center justify-between gap-4">
-        <Link href="/all-events" className="text-sm font-semibold">
-          <Home className="size-5 opacity-75 transition hover:opacity-100" />
-        </Link>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <Home className="size-5 opacity-75 transition hover:opacity-100" />{" "}
+              Welcome, {user ? user.given_name : null} <ChevronDown />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="border border-neutral-800 bg-neutral-900">
+            <DialogHeader className="space-y-6">
+              <div>
+                <DialogTitle className="mb-2">
+                  You are about to log out
+                </DialogTitle>
+                <DialogDescription>
+                  Click the Log out button to log out, or click Cancel to
+                  continue browsing
+                </DialogDescription>
+              </div>
+
+              <div className="flex flex-wrap items-center justify-end gap-x-4">
+                <DialogClose asChild>
+                  <Button variant="secondary">Cancel</Button>
+                </DialogClose>
+                <Button asChild variant="destructive">
+                  <LogoutLink>Log out</LogoutLink>
+                </Button>
+              </div>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
 
         <nav>
           <ul className="flex flex-wrap items-center justify-center gap-4">
