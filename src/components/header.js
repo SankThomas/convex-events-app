cb7d2e2fc57abc6bb23738f89406103d6897413a
SankThomas <thomassankara74@gmail.com>
2024-12-07 12:25:33 +0300

    update links, add z-index to header, update active button variants

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ import { Home } from "lucide-react";
 const links = [
   {
     title: "All Events",
-    href: "/",
+    href: "/all-events",
   },
   {
     title: "Create Event",
@@ -21,9 +21,9 @@ export default function Header() {
   const pathname = usePathname();
 
   return (
-    <header className="fixed left-1/2 mx-auto mt-6 w-full max-w-4xl -translate-x-1/2 rounded-full border border-neutral-700 bg-neutral-800 px-6 py-3">
+    <header className="fixed left-1/2 z-10 mx-auto mt-6 w-full max-w-4xl -translate-x-1/2 rounded-full border border-neutral-700 bg-neutral-800/50 px-6 py-3 backdrop-blur-md">
       <div className="flex flex-wrap items-center justify-between gap-4">
-        <Link href="/" className="text-sm font-semibold">
+        <Link href="/all-events" className="text-sm font-semibold">
           <Home className="size-5 opacity-75 transition hover:opacity-100" />
         </Link>
 
@@ -33,7 +33,7 @@ export default function Header() {
               <li key={link.title}>
                 <Button
                   asChild
-                  variant={`${link.href === pathname ? "default" : "secondary"}`}
+                  variant={`${link.href === pathname ? "secondary" : "default"}`}
                   size="sm"
                 >
                   <Link href={link.href}>{link.title}</Link>
